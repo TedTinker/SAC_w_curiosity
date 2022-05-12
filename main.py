1aebc23c2fe93b705c31f72ca894f2893e2ec671
TedTinker <tedjtinker@gmail.com>
2022-05-12 09:16:04 +0900

    trying again

--- main.py
+++ main.py
@@ -16,27 +16,27 @@ def SAC(n_episodes=200, max_t=500, print_every=10):
         [], [], [], [], []
 
     for i_episode in range(1, n_episodes+1):
-        state = env.reset()
-        state = state.reshape((1,state_size))
+        obs = env.reset()
+        obs = obs.reshape((1,obs_size))
         score = 0
         hidden = None
         trans_losses_, alpha_losses_, actor_losses_, critic1_losses_, critic2_losses_ = \
             [], [], [], [], []
         for t in range(max_t):
-            if(keyboard.is_pressed('q')): env.render()
-            action, hidden = agent.act(state, hidden)
+            #if(keyboard.is_pressed('q')): env.render()
+            action, hidden = agent.act(obs, hidden)
             action_v = action.numpy()
             action_v = np.clip(action_v*action_high, action_low, action_high)
-            next_state, reward, done, info = env.step(action_v)
-            next_state = next_state.reshape((1,state_size))
+            next_obs, reward, done, info = env.step(action_v)
+            next_obs = next_obs.reshape((1,obs_size))
             trans_loss, alpha_loss, actor_loss, critic1_loss, critic2_loss = \
-                agent.step(state, action, reward, next_state, done, t)
+                agent.step(obs, action, reward, next_obs, done, t)
             trans_losses_.append(trans_loss)
             alpha_losses_.append(alpha_loss)
             actor_losses_.append(actor_loss)
             critic1_losses_.append(critic1_loss)
             critic2_losses_.append(critic2_loss)
-            state = next_state
+            obs = next_obs
             score += reward
 
             if done:
@@ -73,10 +73,10 @@ if __name__ == "__main__":
     env = gym.make(args.env)
     action_high = env.action_space.high[0]
     action_low = env.action_space.low[0]
-    state_size = env.observation_space.shape[0]
+    obs_size = env.observation_space.shape[0]
     action_size = env.action_space.shape[0]
     agent = Agent(
-        state_size=state_size, 
+        obs_size=obs_size, 
         action_size=action_size,
         hidden_size=args.hidden_size,
         encode_size=args.encode_size,
